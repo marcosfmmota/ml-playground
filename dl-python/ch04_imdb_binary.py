282bc1cacc5476753e5423ea66bfb040137407d1
marcosfmmota <marcosfmmota@gmail.com>
2024-12-29 04:13:36 +0000

    feat(dl-py): ch04 imdb binary sentiment classification

--- dl-python/ch04_imdb_binary.py
+++ dl-python/ch04_imdb_binary.py
@@ -5,7 +5,10 @@ os.environ["KERAS_BACKEND"] = "jax"
 
 import keras
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.datasets import imdb
+from keras import layers
+
 
 # %%
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
@@ -35,4 +38,49 @@ x_train = multi_hot_encoding(train_data, num_classes=10000)
 x_test = multi_hot_encoding(test_data, num_classes=10000)
 y_train = np.asarray(train_labels).astype("float32")
 y_test = np.asarray(test_labels).astype("float32")
+# %%
+model = keras.Sequential(
+    [
+        layers.Dense(16, activation="relu"),
+        layers.Dense(16, activation="relu"),
+        layers.Dense(1, activation="sigmoid"),
+    ]
+)
+
+# %%
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+history = model.fit(x_train, y_train, epochs=20, batch_size=512, validation_split=0.2)
+# %%
+history_dict = history.history
+loss_values = history_dict["loss"]
+val_loss_values = history_dict["val_loss"]
+epochs = range(1, len(loss_values) + 1)
+plt.plot(epochs, loss_values, "r--", label="Training loss")
+plt.plot(epochs, val_loss_values, "b", label="Validation loss")
+plt.title("[IMDB] Training and validation loss")
+plt.xlabel("Epochs")
+plt.xticks(epochs)
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+# %%
+plt.clf()
+acc = history_dict["accuracy"]
+val_acc = history_dict["val_accuracy"]
+plt.plot(epochs, acc, "r--", label="Training acc")
+plt.plot(epochs, val_acc, "b", label="Validation acc")
+plt.title("[IMDB] Training and validation accuracy")
+plt.xlabel("Epochs")
+plt.xticks(epochs)
+plt.ylabel("Accuracy")
+plt.legend()
+plt.savefig("imdb_accuracy_plot.png", dpi=300)
+plt.show()
+
+# %%
+# Since the model overfit after 4 epochs we retrain with only 4 epochs and evaluate on test data.
+model.fit(x_train, y_train, epochs=4, batch_size=512)
+results = model.evaluate(x_test, y_test)
+print(f"Test set loss {results[0]} - accuracy {results[1]}")
 # %%
