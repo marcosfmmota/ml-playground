a44b7b63d9fc40ced4ca009230e2642740a96045
Marcos Mota <marcosfmmota@gmail.com>
2025-04-22 22:30:09 -0300

    feat(dl-py): ch07 custom train loop in pytorch

---
+++ dl-python/ch07_train_loop_torch.py
@@ -0,0 +1,54 @@
+# %%
+import os
+
+os.environ["KERAS_BACKEND"] = "torch"
+
+import keras
+import torch
+from keras import layers
+from keras.datasets import mnist
+
+
+# %%
+def get_mnist_model():
+    inputs = keras.Input(shape=(28 * 28,))
+    features = layers.Dense(64, activation="relu")(inputs)
+    features = layers.Dropout(0.5)(features)
+    outputs = layers.Dense(10, activation="softmax")(features)
+    model = keras.Model(inputs, outputs)
+    return model
+
+
+# %%
+
+model = get_mnist_model()
+loss_fn = keras.losses.SparseCategoricalCrossentropy()
+optimizer = keras.optimizers.Adam()
+
+
+def train_step(inputs, targets):
+    predictions = model(inputs, training=True)
+    loss = loss_fn(targets, predictions)
+    loss.backward()
+    gradients = [weight.value.grad for weight in model.trainable_weights]
+    with torch.no_grad():
+        optimizer.apply(gradients, model.trainable_weights)
+    model.zero_grad()
+    return loss
+
+
+# %%
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype("float32") / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype("float32") / 255
+# run on a batch
+batch_size = 32
+inputs = train_images[:batch_size]
+targets = train_labels[:batch_size]
+loss = train_step(inputs, targets)
+
+# %%
+print(loss)
+# %%
